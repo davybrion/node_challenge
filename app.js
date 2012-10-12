13fd5ba62ffb1bd16bf341dd48d6a570bfb928ae
Davy Brion <ralinx@davybrion.com>
2012-10-12 11:47:11 +0200

    very basic input validation on query string params... just checking for presence atm

--- app.js
+++ app.js
@@ -2,8 +2,33 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
+	var width = req.query.w,
+		height = req.query.h,
+		url = req.query.u;
+
+	validateQueryStringParameters(width, height, url, res);
+
   res.send('Hello World');
 });
+
+var validateParameter = function(parameter, name, res) {
+	if (!parameter) {
+		res.send("you have to provide a '" + name + "' parameter in the query string");
+		return false;
+	};
+	return true;
+}
+
+var validateQueryStringParameters = function(width, height, url, res) {
+	var valid = true;
+	
+	valid = validateParameter(width, 'w', res) 
+		&& validateParameter(height, 'h', res)
+		&& validateParameter(url, 'u', res);
+
+	return valid;
+};
+
 
 app.listen(3000);
 console.log('Listening on port 3000');
